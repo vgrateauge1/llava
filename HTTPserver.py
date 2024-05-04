40444b2c709c4582f7952f624dec6c453994387c
Val <valentin.grateau1309@gmail.com>
2024-05-04 18:12:41 +0200

    delete + bug correction + switch to json or not format for answers

--- HTTPserver.py
+++ HTTPserver.py
@@ -1,65 +1,69 @@
-from flask import Flask, request, render_template,redirect, url_for,session,jsonify
-from llava import *
-
+from flask import Flask, request, jsonify, render_template, session, url_for, redirect
+from werkzeug.utils import secure_filename
+import logging
 import os
+import uuid
+import llava
 
 app = Flask(__name__)
-app.secret_key = 'JaiPlusDeVoix'  # Définissez une clé secrète forte
-
-llava = createLlava()
-# Ensure there's a folder named 'uploads' in the same directory as this script
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'JaiPlusDeVoix'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/submit-text', methods=['POST'])
 def handle_text():
     text = request.form['text']
-    # Process the text as needed, e.g., save to a file or database
-    print(text)  # For demonstration purposes
-
-    return redirect(url_for('index'))  # Redirect back to the main page
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    message = ""
-    if request.method == 'POST':
-        # Assuming your input field's name is 'text'
-        question = request.form['text']
-        if 'uploaded_file_path' in session:
-            file_path = session['uploaded_file_path']
-            print(file_path)
-            jsonAnswer, message = analyze_image(file_path)
-
+    json_format = 'jsonFormat' in request.form
+    
+    logging.debug(f"Received text: {text}")
+    if 'uploaded_file_path' in session:
+        file_path = session['uploaded_file_path']
+        logging.debug(f"Analyzing image at {file_path} with text: {text}")
+        if json_format:
+            jsonAnswer, llmanswer = llava.analyze_image(file_path, text)
             session.pop('uploaded_file_path', None)
+            return render_template('index.html', message=jsonAnswer)  # Return JSON formatted response
         else:
-            message = askLlava(llava,question)
+            llmanswer = llava.analyze_criteria_image(file_path, text)
+            session.pop('uploaded_file_path', None)
+            return render_template('index.html', message=llmanswer)  # Return plain text response
+    else:
+        # Handle cases where there's no image uploaded but text is submitted
+        message = "No image uploaded to analyze with text."
+        return render_template('index.html', message=message)
 
-        
-        print(jsonAnswer)
-        # You can pro cess the message here if needed
-    return render_template('index.html', message=message)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', message="Upload an image and enter some text.")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part', 400
+        return jsonify({'error': 'No file part'}), 400
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file', 400
-    if file:
-        filename = file.filename
-        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(save_path)
-        absolute_path = os.path.abspath(save_path)
-        # Stocker le chemin du fichier dans la session
-        session['uploaded_file_path'] = absolute_path
-        return jsonify({'message': 'File uploaded successfully', 'path': absolute_path})
-    else:
-        return 'Upload failed', 500
+        return jsonify({'error': 'No selected file'}), 400
+    filename = secure_filename(file.filename)
+    file_id = str(uuid.uuid4())
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(save_path)
+    session['uploaded_file_path'] = save_path  # Store the file path in the session
+    return jsonify({'message': 'File uploaded successfully', 'path': save_path, 'fileId': file_id})
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+@app.route('/delete-file', methods=['POST'])
+def delete_file():
+    file_id = request.form.get('id')
+    if file_id:
+        target_path = os.path.join(app.config['UPLOAD_FOLDER'], file_id)
+        if os.path.exists(target_path):
+            os.remove(target_path)
+            return jsonify({'success': True})
+        else:
+            return jsonify({'success': False}), 404
+    return jsonify({'success': False}), 400
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- llava.py
+++ llava.py
@@ -33,15 +33,6 @@ def plt_img_base64(img_base64):
     # Display the image by rendering the HTML
     display(HTML(image_html))
 
-def createLlava():
-    llm = Ollama(model="llava")
-    return llm
-
-
-def askLlava(question,image):
-    message = bakllava.invoke(question)
-    return message
-
 def convert_to_base64(pil_image):
     """
     Convert PIL images to Base64 encoded strings.
@@ -59,32 +50,33 @@ def analyze_criteria_image(file_path, question):
     :return: LLM's response
     """
 
-    # Extracting the file path from the tuple
-
-    # Using os.path.basename to extract the file name
-#        image_name = os.path.basename(file_path)
-#        print("DEBUG "+image_name)
     pil_image = Image.open(file_path)
     image_b64 = convert_to_base64(pil_image)
     llm_with_image_context = bakllava.bind(images=[image_b64])
     return llm_with_image_context.invoke(question)
 
-def analyze_image(file_path):
+def analyze_image(file_path,prompt):
     """
-    Analyzes an image and returns the LLM's response.
+    Analyzes an image and returns the LLM's response after using regex to extract json values.
     :param file_path: Path to the image file
     :return: LLM's response
     """
-    prompt ="""Extract the following information: hair color, age, skin color and gender. """
-    prompt+="""Format the results in JSON text, ensuring to include the fields \"hairColor\", \"age\", \"skinColor\" and \"gender\". """
-    prompt+="""The expected result characteristics are: """
-    prompt+="""The results must be accurate and reliable."""
-    prompt+="""Gender values should Male or Female. """
-    prompt+="""age should be a number. """
-    prompt+="""skinColor should be one of this values Dark, Ebony, Ivory, Light, Medium or Unknown """
-    prompt+="""hairColor should be one of this values  Black, Blonde, Brown, Gray, Red, White or Unknown. """
+    # First Prompt:
+    # Extract information about hair color, age, skin color, and gender from the data. The results should be formatted in JSON, including fields for 'hairColor', 'age', 'skinColor', and 'gender'. Each result must be accurate and reliable, with gender categorized as Male or Female. The age should be represented as a number. Skin color options include Dark, Ebony, Ivory, Light, Medium, or Unknown. Hair colors should be identified as Black, Blonde, Brown, Gray, Red, White, or Unknown. Don't add any comments or precision in the JSON but only the fields asked.
 
-    resultllm = analyze_criteria_image(file_path,prompt)
+    # Second Prompt:
+    # Analyze an image of food and identify all visible ingredients. Use this information to generate a list of possible dishes that can be made from these ingredients. Present the results in JSON format, categorizing them under two primary keys: 'ingredients' and 'possibleDishes'. Under 'ingredients', list each detected food item, and under 'possibleDishes', suggest names of dishes that could be prepared using these ingredients. Ensure that the ingredient detection is precise, and the dish suggestions are practical, reflecting common culinary practices. Don't add any comments or precision in the JSON but only the fields asked.
+
+    # Third Prompt:
+    # Conduct a detailed analysis of the clothing worn by a person in an image. Identify each article of clothing, noting its type, such as t-shirt, sweater, hoodie, or jeans. Specify the color of each item using precise descriptors like 'baby blue', 'dark green', and so on. Include any additional elements or distinctive features on the clothing, such as patterns, logos, or text. Structure the results as a list, with each entry detailing the 'type of clothing', 'color', and 'additional elements'. The description should be thorough, capturing subtle details and providing an accurate representation of the attire. Don't add any comments or precision in the JSON but only the fields asked.
+
+    # Fourth Prompt:
+    # Meticulously examine an image depicting a car accident. Count and confirm the total number of vehicles involved. Assess the severity of the accident using a scale from 'not serious' to 'very serious'. Format the analysis results in JSON, including keys for 'numberOfVehicles', with an integer value representing the count of vehicles, and 'accidentSeverity', with a string value describing the severity of the accident. Ensure the JSON output is precise and the assessment accurately reflects the visible details of the accident. The information should be presented clearly for straightforward interpretation. Don't add any comments or precision in the JSON but only the fields asked.
+    
+    if file_path is None:
+        resultllm = askLlava(bakllava,prompt)
+    else:
+        resultllm = analyze_criteria_image(file_path,prompt)
 
     # Utilisation d'une expression régulière pour trouver tout ce qui est entre {}
     match = re.search(r'\{(.*?)\}', resultllm, re.DOTALL)
@@ -95,31 +87,7 @@ def analyze_image(file_path):
         result = "{"+match.group(1)+"}"
     else:
         print("Aucun contenu trouvé entre accolades.")
-    print("Final Result from LLM : "+result)
-#        age= self.analyze_criteria_image(file_path,"what is the age  ? Reply only the age")
-#        skin = self.analyze_criteria_image(file_path,"what is the color of skin  ? Reply only Clear or Medium or Dark ")
-#        gender = self.analyze_criteria_image(file_path,"Is it a woman or a man  ? Reply only by Woman or Man")
-#        return dict(hair_color=hair_color,age=age,skin=skin,gender=gender)        
-#        age=32
-#        skin="Clear"
-#        gender="Female"
+    print("Final Result from LLM : " + result)
     return dict(extractedPictureElements=json.loads(result)),resultllm
 
-def fake_analyse(self, file_path):
-    """
-    Analyzes an image and returns the LLM's response.
-    :param file_path: Path to the image file
-    :return: LLM's response
-    """
-#        hair_color = self.analyze_criteria_image(file_path,"what is the hair color ? Reply only the color")
 
-#        age= self.analyze_criteria_image(file_path,"what is the age  ? Reply only the age")
-
-#        skin = self.analyze_criteria_image(file_path,"what is the color of skin  ? Reply only Clear or Medium or Dark ")
-
-#        gender = self.analyze_criteria_image(file_path,"Is it a woman or a man  ? Reply only by Woman or Man")
-    hair_color="Red"
-    age=5
-    skin="Light"
-    gender="Female"
-    return dict(extractedPictureElements=dict(hairColor=hair_color,age=age,skinColor=skin,gender=gender))
